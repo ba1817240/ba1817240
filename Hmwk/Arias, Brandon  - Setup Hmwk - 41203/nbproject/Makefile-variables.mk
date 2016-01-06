cb8167bdb2eb422dd6979b31d9a27ff30e824eff
Brandon Arias <Brandon Arias>
2016-01-06 01:32:11 -0800

    Signed-off-by: Brandon Arias <Brandon Arias>

---
+++ Hmwk/Arias, Brandon  - Setup Hmwk - 41203/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=arias__brandon__-_setup_hmwk_-_41203
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/arias__brandon__-_setup_hmwk_-_41203
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=arias,brandon-setuphmwk-41203.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/arias,brandon-setuphmwk-41203.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=arias__brandon__-_setup_hmwk_-_41203
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/arias__brandon__-_setup_hmwk_-_41203
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=arias,brandon-setuphmwk-41203.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/arias,brandon-setuphmwk-41203.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
