c0ab97ca47782a61cf6de5908bd144128c926135
Brandon Arias <barias3@student.rcc.edu>
2016-01-10 16:31:16 -0800

    Gaddis_8thEd_Chap2_Prob15
    Assignment 1

---
+++ Hmwk/Assignment 1/Gaddis_8thEd_Chap2_Prob15/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * File:   main.cpp
+ * Author: Brandon Arias
+ * Created on January 10, 2016, 4:25 PM
+ * Purpose: Make a Triangle
+ */
+
+//System Libraries
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv)
+
+{
+    //Display triangle
+    cout << "    *     " << endl;
+    cout << "   ***    " << endl;
+    cout << "  *****   " << endl;
+    cout << " *******  " << endl;
+
+    //End program
+
+    return 0;
+
+}
